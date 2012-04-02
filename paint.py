47aac1f93452f903263ea9983184ff4fbb082c0f
Marek Rogalski <mafikpl@gmail.com>
2012-04-02 11:37:16 +0200

    Revert "Some old changes"
    This reverts commit 031f12bfaf5562c2dad6f71bc380747c780e4e7c.

--- paint.py
+++
@@ -1,8 +0,0 @@
-from clutter import cogl
-
-def rect(x1, y1, x2, y2, color):
-    cogl.path_rectangle(x1, y1, x2, y2)
-    cogl.path_close()
-
-    cogl.set_source_color(color)
-    cogl.path_fill()
